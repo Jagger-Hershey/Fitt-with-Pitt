5e4271253a0259c57a7c085a9addae93a0e81f9e
Jagger-Hershey <jch168@pitt.edu>
2026-03-24 17:36:25 -0400

    Initial flask login setup, reworked routes, basic html templates

--- app.py
+++ app.py
@@ -1,22 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from models import db
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from models.user_model import User
 from models.nutrition_model import Nutrition
 from models.activity_model import Activity
 
+# Initialize flask application and configure database
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize database and login manager
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Please log in to access this page.'
+login_manager.login_message_category = 'info'
 
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return redirect(url_for('login'))
 
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check user against database hashed info
+        # If authorized, login and redirect to home
+        # Otherwise throw login error
+    else:
+        return render_template('login.html')    
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check if username is unique throw error if not
+        # Otherwise create user and add to database
+        # Redirect to login
+
+    else:
+        return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
